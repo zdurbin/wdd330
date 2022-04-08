7f59e15743fa18af1e1232dbba72377449cad835
zdurbin <dur15014@byui.edu>
2022-04-07 17:14:17 -0700

    final updates

--- challenge 2/calorie-counter.js
+++ challenge 2/calorie-counter.js
@@ -1,3 +1,21 @@
+//const axios = require("axios");
+//
+//const options = {
+//  method: 'GET',
+//  url: 'https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese',
+//  params: {fields: 'item_name,item_id,brand_name,nf_calories,nf_total_fat'},
+//  headers: {
+//    'X-RapidAPI-Host': 'nutritionix-api.p.rapidapi.com',
+//    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+//  }
+//};
+//
+//axios.request(options).then(function (response) {
+//	console.log(response.data);
+//}).catch(function (error) {
+//	console.error(error);
+//});
+
 var maxItems = 20;
 var initItemAmount = 10; //this is the intial amount of input rows created in the HTML
 var buttonAmount = 1;
@@ -97,3 +115,47 @@ function reset() {
   document.getElementById("totalCalories").innerHTML = 0;
   document.getElementById("Add").style.visibility = "visible";
 }
+
+//// Listen to form submit commented out due to unresolved issues with calorie counter
+//document.getElementById('myForm').addEventListener('submit', getValues);
+//// Get form value
+//function getValues(e){
+//  var height = document.getElementById('height').value;
+//  var weight = document.getElementById('weight').value;
+//  var age = document.getElementById('age').value;
+//  var radios = document.getElementsByName('gender');
+//  var gender;
+//  //Loops through radio buttons
+//  for(var i = 0; i < radios.length; i++){
+//    if(radios[i].checked){
+//      gender = radios[i].value;
+//    }
+//  }
+//  
+//  calculateBmr(height, weight, age, gender);
+//  e.preventDefault();
+//}
+//// Calculate Basal Metabolic Rate 
+//function calculateBmr(height, weight, age, gender){
+//  var bmr;
+//  
+//  //Formulas are from http://www.bmi-calculator.net/bmr-calculator/bmr-formula.php
+//  if(gender == 'male'){
+//    bmr = 66 + (6.23*weight) + (12.7*height) - (6.8*age);
+//  }else if(gender == 'female'){
+//    bmr = 655 + (4.35*weight) + (4.7*height) - (4.7*age);
+//  }else{
+//    alert("Please select a gender!");
+//  }
+//  
+//  displayBmr(bmr);
+//}
+//
+//function displayBmr(bmr){
+//  //Get output id
+//  var bmrResults = document.getElementById('bmrResults');
+//  bmrResults.innerHTML = "<h3>Your Basal Metabolic Rate is: " + bmr+ "</h3>";
+//}
+
+
+
